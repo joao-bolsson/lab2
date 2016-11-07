e4191b88bb077fc2c9f771eea0cbcdfb34f244e8
NCC <ncc@debian-desktop-ncc>
2016-11-07 18:31:11 -0200

    implementando contagem e ocorrencias

--- Indexador/main.cpp
+++ Indexador/main.cpp
@@ -15,24 +15,45 @@ struct Posicao {
 
 struct Palavra {
   string palavra; // a palavra
-  //int contagem;        // palavras encontradas
-  //list<Posicao> lista; // lista com a posição de cada palavra
+  int contagem;        // palavras encontradas
+  list<Posicao> lista; // lista com a posição de cada palavra
 };
 
 class No {
 private:
 	No *esq, *dir;
-  string palavra;
+    Palavra palavra;
 
 public:
-	No(string palavra) {
+	No(Palavra palavra) {
 		this->palavra = palavra;
 		esq = NULL;
 		dir = NULL;
 	}
 
-	string getPalavra() {
-		return palavra;
+	string toString() {
+		return palavra.palavra;
+	}
+
+  int getContagem() {
+    return palavra.contagem;
+  }
+
+	void updateContagem() {
+    palavra.contagem++;
+	}
+
+	void updateLista(Posicao pos) {
+    palavra.lista.push_back(pos);
+	}
+
+	void printLista() {
+    cout << "(";
+    for (auto it = palavra.lista.begin(); it != palavra.lista.end(); it++) {
+      Posicao pos = *it;
+      cout << pos.linha << ":" << pos.coluna << ", ";
+    }
+    cout << ")\n";
 	}
 
 	No* getEsq() {
@@ -56,21 +77,17 @@ class Arvore {
 private:
 	No *raiz;
 
-	void inserirAux(No *no, string chave) {
-		// se for menor, então insere à esquerda
-		if(chave < no->getPalavra()) {
+	void inserirAux(No *no, Palavra chave) {
+		if(chave.palavra < no->toString()) {
 			// verifica se a esquerda é nulo
 			if(no->getEsq() == NULL) {
 				No *novo_no = new No(chave);
 				no->setEsq(novo_no); // seta o novo_no à esquerda
-			}
-			else {
+			} else {
 				// senão, continua percorrendo recursivamente
 				inserirAux(no->getEsq(), chave);
 			}
-		}
-		// se for maior, então insere à  direita
-		else if(chave > no->getPalavra()) {
+		} else if(chave.palavra > no->toString()) {
 			// verifica se a direita é nulo
 			if(no->getDir() == NULL) {
 				No *novo_no = new No(chave);
@@ -79,6 +96,10 @@ private:
 				// senão, continua percorrendo recursivamente
 				inserirAux(no->getDir(), chave);
 			}
+		} else if (chave.palavra == no->toString()) {
+      /// se a palavra já tiver na arvore, aumenta a contagem
+      no->updateContagem();
+      no->updateLista(chave.lista.back());
 		}
 		// se for igual, não vai inserir
 		// não pode existir 2 chaves iguais
@@ -89,7 +110,7 @@ public:
 		raiz = NULL;
 	}
 
-	void inserir(string chave) {
+	void inserir(Palavra chave) {
 		if(raiz == NULL) // verifica se a árvore está vazia
 			raiz = new No(chave); // cria um novo nó
 		else
@@ -104,7 +125,8 @@ public:
     //Primeiro a sub-árvore esquerda, depois a raiz, e finalmente a sub-árvore direita
 		if(no != NULL) {
 			emOrdem(no->getEsq());
-			cout << no->getPalavra() << " ";
+			cout << no->toString() << " " << no->getContagem();
+			no->printLista();
 			emOrdem(no->getDir());
 		}
 	}
@@ -129,15 +151,25 @@ Arvore le_arquivo(const char* nome) {
   string line;
   ifstream myfile (nome);
   if (myfile.is_open()) {
+    int line_cont = 1; /// contagem das linhas no arquivo
     while(!myfile.eof()) {
       getline (myfile,line);
-      if (line.size() >= 1) { /// apenas linhas com conteudo
-        vector<string> v{explode(line, ' ')};
-        int tam = v.size();
-        for (int i = 0; i < tam; i++) {
-          arv.inserir(v[i]);
+      vector<string> v{explode(line, ' ')};
+      int tam = v.size();
+      Posicao pos;
+      pos.linha = line_cont;
+      pos.coluna = 1;
+      for (int i = 0; i < tam; i++) {
+        Palavra word;
+        if (i != 0) {
+          pos.coluna = v[i - 1].size() + 1;
         }
+        word.palavra = v[i];
+        word.contagem = 1;
+        word.lista.push_back(pos);
+        arv.inserir(word);
       }
+      line_cont++;
     }
     myfile.close();
   } else {
