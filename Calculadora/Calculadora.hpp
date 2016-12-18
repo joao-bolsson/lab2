d3fba629e6565ea756a736cfe769f29bb0f0002f
Joao Bolsson <joaovictorbolsson@gmail.com>
2016-12-18 13:44:29 -0200

    Iniciando implementacao do t9

---
+++ Calculadora/Calculadora.hpp
@@ -0,0 +1,97 @@
+// Calculadora.hpp
+// TAD que implementa uma calculadora
+//
+// The MIT License (MIT)
+// 
+// Copyright (c) 2016 João V. Lima, UFSM
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#pragma once 
+
+#include <stack>
+
+template<typename T>
+struct Calculadora {
+    // ver http://en.cppreference.com/w/cpp/container/stack
+    std::stack<T> operandos; // números, basicamente
+    std::stack<char> operadores; // operadores: +, -, /, *
+    // podem incluir mais dados, se necessário
+
+    // cria a calculadora
+
+    void cria(void) {
+    }
+
+    // destroi a calculadora, limpa as pilhas
+
+    void destroi(void) {
+        while (operandos.empty() == false)
+            operandos.pop();
+        while (operadores.empty() == false)
+            operadores.pop();
+    }
+
+    // insere um novo operando para o calculo
+
+    void operando(T op) {
+        operandos.push(op);
+    }
+
+    // insere um novo operador para o calculo (pode ser '+', '-', '*', '/', '^')
+    // se for um abre parenteses '(' tudo ok. 
+    // se for um fecha parenteses ')', deve-se calcular tudo dentro.
+
+    void operador(char op) {
+        if (op == ')') {
+
+        }
+        // verficar aqui o operador inserido
+        operadores.push(op);
+    }
+
+    float divide(float a, float b) {
+        return a / b;
+    }
+
+    float multiplica(float a, float b) {
+        return a * b;
+    }
+
+    float subtrai(float a, float b) {
+        return a - b;
+    }
+
+    float soma(float a, float b) {
+        return a + b;
+    }
+
+    // finaliza o calculo, retorna false se erro detectado
+
+    bool fim(void) {
+        return false;
+    }
+
+    // retorna o resultado calculo (topo da pilha de operandos)
+    // esse resultado sera parcial se ainda nao foi chamado calc_fim().
+
+    T resultado(void) {
+        return operandos.top();
+    }
+};
---
+++ Calculadora/main.cpp
@@ -0,0 +1,30 @@
+/**
+ * Implementado no NetBeans 8.2, compilado com:
+ * 
+ * make -f Makefile CONF=Debug (raiz do projeto)
+ * 
+ * @author João Bolsson (jvmarques@inf.ufsm.br)
+ * @since 2016, 18 Dez.
+ */
+
+#include "Calculadora.hpp"
+#include <iostream>
+
+int main() {
+    Calculadora<float> calc;
+    float x;
+
+    calc.operador('(');
+    calc.operando(3.0);
+    calc.operador('+');
+    calc.operando(6.0);
+    calc.operador(')');
+
+    if (calc.fim() == false) {
+        std::cout << "ERRO: formula errada!" << std::endl;
+    }
+    x = calc.resultado();
+    std::cout << "EXEMPLO: (3+6) = " << x << std::endl;
+    calc.destroi();
+    return 0;
+}
